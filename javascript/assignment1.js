e9c2494bbe2e485611df4b1c1f748143ff0cd77e
nugenthomas <nguyn880@newschool.edu>
2015-09-25 18:26:43 -0400

    hi

--- javascript/assignment1.js
+++ javascript/assignment1.js
@@ -1,4 +1,23 @@
-// ==============
-//  Assignment 1
-// ==============
+var x = "We're No Stranger to love you. You know the rules and so do I. A full commitment's what I'm thinking of. You wouldn't get this from any other guy. I just wanna tell you how I'm feeling. Gotta make you understand. Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you";
 
+var y = ". Rick Astley, July 1987";
+var y = y.toUpperCase();
+
+var res = x.slice(0,9);
+
+var res = x.match(/yo/g);
+
+var x = x+y;
+
+var z = "You're about to be rick rolled";
+
+var xes = res.concat(z);
+
+
+x = x.replace("stranger", "loser");
+x = x.replace("rules", "guy");
+
+var x = x.replace(/Never/g, "Always");
+
+console.log(xes);
+console.log(x);
--- javascript/assignment3.js
+++ javascript/assignment3.js
@@ -1,4 +1,50 @@
-// ==================
-//  Assignment 3
-// ==================
+var colorArray = ["blue","red","green","purple","yellow"];  
+var Yposition = 0;
+var Xposition = 0;
 
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+    
+
+  function draw() {
+background("lightblue");
+    Yposition = Yposition + 3;
+
+    if (Yposition > windowHeight) {
+      Yposition = 0 
+    }
+
+    //for (var x = 0; x < windowWidth; x++){
+   
+    //ellipse(x,0,50,50);
+if (mouseIsPressed) {
+  background(random(0,10));
+  Yposition -=4;
+  colorMode (HSB, 100);
+
+      if (Yposition < 0) {
+        Yposition = windowHeight+ random(-4);  
+      }}
+    for (var x=0;x<100;x++){
+            fill(random(0,255),random(0,255),random(0,255));
+
+    ellipse(x*100+1, Yposition, random(1,50),random(1,50));
+
+  for(var y = 0; y<colorArray.length;y++){
+        fill(colorArray[y]);
+          rect(Yposition, x * 100+1, random(1,30),random(1,30));}
+
+    //for (var y = 0; y < 5; y++) {
+      //fill(y*30,150,200);
+    //rect(x*100+10, Yposition, 70, 70);
+}    
+    //Xposition = Xposition +3
+    //if (Xposition > windowHeight){
+     // Xposition = 0;}
+    //for (var x=0;x<100;x++){
+    //ellipse(x*100+10, Xposition, random(1,50),random(1,50));
+    //triangle(Xposition, 75, Xposition
+      //, 20, Xposition, 75);
+
+  }

eb282cea6b95a847b7e9dda8247e0cd2b86f173b
griffinjameskeller <kellg428@newschool.edu>
2015-10-02 15:45:34 -0400

    portfolio-update

--- javascript/assignment2.js
+++ javascript/assignment2.js
@@ -1,4 +1,129 @@
-// ===============
-//  Assignment 2
-// ===============
+function setup() {
 
+	//==============================
+  //  START writing your code here
+  //==============================
+	//make the canvas the full size of the window
+	  createCanvas(800, 900);
+
+	  //set the background color
+	  background('#92e8f2');
+
+	  noStroke();
+	  // rectangle:
+	  // (x coorditante, y coordinate, width, height)
+	  fill('white')
+
+	  ellipse(470, 390, 400, 400);
+
+	  fill('#92e8f2')
+
+	  ellipse(470, 390, 370, 370);
+
+	   fill('white')
+
+	  ellipse(470, 390, 350, 350);
+
+	  fill ('#92e8f2')
+
+	  rect(450,90,20,700);
+
+	  fill ('#eaabc3')
+
+	  rect(400,90,60,50);
+
+	  fill ('#af5778')
+
+	  rect(450,90,10,50);
+
+	  fill ('#984262')
+
+	  rect(455,90,5,50);
+
+	  fill ('#c47090')
+
+	  rect(430,90,20,50);
+
+	  fill ('#d88ba8')
+
+	  rect(413,90,27,50);
+
+	  fill ('white')
+
+	  rect(405,95,5,20);
+
+	  fill ('#888888')
+
+	  rect(398,130,30,20);
+
+
+	  fill ('#4f4f4f')
+
+	  rect(442,130,20,20);
+
+	  fill ('#646464')
+
+	  rect(415,130,30,50);
+
+	  fill('#ffe400')
+
+	  rect(400,150,20,500);
+
+
+	  fill('#e39b00')
+
+	  rect(440,150,20,500);
+
+	  fill('#efbf00')
+
+	  rect(415,150,30,500);
+
+	  fill ('#c8c8c8')
+
+	  rect(405,135,5,7);
+
+	  fill ('white')
+
+	  rect(406,165,3,350);
+
+	  // ellipse:
+	  // (coorditante, y coordinate, width, height)
+
+	  // circle: is an ellipses with equal width and height
+	  // (coorditante, y coordinate, width, height)
+	  //ellipse(400, 100, 80, 80);
+
+	  // line
+	  // coordinates of points: x1,y1,x2,y2
+
+	  // triangle
+	  // coordinates of points: x1,y1,x2,y2,x3,y3
+
+	  fill('#e7a980')
+
+	  triangle(460, 650, 400, 650, 430, 730);
+
+	  fill('#cf804b')
+
+	  triangle(460, 650, 425, 650, 430, 730);
+
+	  fill('#e29663')
+
+	  triangle(445, 650, 415, 650, 430, 730);
+
+	fill('black')
+
+	  triangle(440, 702, 420, 702, 430, 730);
+
+
+	  // points
+	  // (x coordinate, y coordinate)
+	  point(300, 200);
+	  point(350, 250);
+
+
+
+	//==============================
+  //  STOP writing your code here
+  //==============================
+}
--- javascript/assignment3.js
+++ javascript/assignment3.js
@@ -1,4 +1,34 @@
-// ==================
-//  Assignment 3
-// ==================
+var Xposition = 0;
+var Yposition = 0;
+var Ybottom = 400;
 
+function setup() {
+  createCanvas(1440, 900);
+  background (0);
+
+
+}
+
+function draw() {
+
+
+
+    stroke(mouseX,mouseY,Ybottom);
+    line(mouseX, mouseY, Yposition, Ybottom);
+
+  if (mouseIsPressed) {
+  Yposition = Yposition - 6;
+  Ybottom = Ybottom + 6;
+  } else {
+Yposition = Yposition + 3;
+  }
+
+  if (Yposition > 1440) {
+   Yposition = 0;
+   Xposition = 0;
+    clear();
+  background(0);
+  }
+
+
+}

94754e7a29fb66023e7f5146c7c5f6f59c05a167
Kaisha M <baurmurza@Baurzhan-Murzamadiyevs-MacBook-Pro-2.local>
2015-10-02 00:16:45 -0400

    Put picture in

--- javascript/assignment1.js
+++ javascript/assignment1.js
@@ -2,3 +2,16 @@
 //  Assignment 1
 // ==============
 
+	
+var x = "But even so, every now and then I would feel a violent stab of loneliness. The very water I drink, the very air I breathe, would feel like long, sharp needles. The pages of a book in my hands would take on the threatening metallic gleam of razor blades. I could hear the roots of loneliness creeping through me when the world was hushed at four o'clock in the morning.";
+x=x.replace("violent stab","flush");
+x=x.replace("loneliness","happiness");
+x=x.replace("long, sharp needles","a soft, quiet, ocean breeze");
+x=x.replace("threatening metallic gleam of razor blades","tempting shimmer of crystal rocks");
+x=x.replace("roots of loneliness","chirps of birds");
+x=x.replace("creeping through","encompassing");
+x=x.replace("hushed","awakened");
+x=x.replace("four","10");
+console.log(x);
+
+
--- javascript/assignment2.js
+++ javascript/assignment2.js
@@ -1,4 +1,58 @@
 // ===============
 //  Assignment 2
 // ===============
+function setup (){
 
+	createCanvas(592, 378);
+
+ 
+background('lightblue');
+
+  
+fill("white");
+
+  ellipse(300, 101, 100, 100);
+  fill("black");
+ ellipse(280, 90, 10, 10);
+ ellipse(321, 90, 10, 10);
+
+
+fill("white");
+ellipse(300, 201, 100, 100);
+
+fill("white");
+ellipse(300, 301, 100, 100);
+
+fill("black");
+ellipse(300, 200, 5, 5);
+
+fill("black");
+ellipse(300, 220, 5, 5);
+
+fill("black");
+ellipse(300, 180, 5, 5);
+
+ 
+
+  fill("brown"); 
+  line(339, 190, 410, 125);
+  fill("brown"); 
+  line(259, 190, 170, 125);
+
+  line(0, 351, 600, 351);
+  fill("green");
+  rect(0,351,600,180);
+   fill("white");
+  rect(0,351,600,18);
+
+
+  fill("orange");
+
+  triangle(289, 112, 318, 103, 344, 150);
+
+
+
+
+  point(300, 200);
+  point(350, 250);
+}
